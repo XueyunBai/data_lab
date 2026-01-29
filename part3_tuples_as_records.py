8f9a81487e1aac7d7c5007a08e6239b98cd5de80
XueyunBai <xb2042@nyu.edu>
2026-01-29 15:12:39 -0500

    Update part3_tuples_as_records.py

--- part3_tuples_as_records.py
+++ part3_tuples_as_records.py
@@ -10,10 +10,15 @@ students = [
 # columns: (id, name, school, credits)
 
 # TODO 1: Print the name for each record
-
+for row in students:
+    print (row[1])
 # TODO 2: Try to change Bob’s credits directly (observe the error)
-
+#students[1][3]=30
 # TODO 3: Create a new tuple for Bob with credits=30 and replace the old record in the list
-
+for i in range(len(students)):
+    if students[i][1]=="Bob":
+        students[i]=(students[i][0], students[i][1], students[i][2], 30)
+print(students)
 # Reflection (answer in a comment):
 # TODO: Why might immutability be good for data integrity?
+#since the data cannot be changed accidently
